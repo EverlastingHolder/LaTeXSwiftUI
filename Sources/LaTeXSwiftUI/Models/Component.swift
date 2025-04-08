c6f730480637d5ca4acf83b52b15e9bf79b9cdff
roman.moshkovcev <roman.moshkovcev@myworkflow.ru>
2025-04-08 16:14:20 +0700

    fix: bold

--- Sources/LaTeXSwiftUI/Models/Component.swift
+++ Sources/LaTeXSwiftUI/Models/Component.swift
@@ -260,7 +260,7 @@ extension Component {
         else {
             do {
                 var input = input
-                let count = input.count(where: { $0 == Character("**") })
+                let count = input.count(where: { $0.description == "**" })
                 if count % 2 != 0 {
                     input.append(contentsOf: "**")
                 }
